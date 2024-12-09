9494018539cea23992dca022c731fb5cad162a96
Yasowant <yasowant1998@gmail.com>
2024-12-09 23:53:10 +0530

    courses route also fixed

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
 import { addCourse, getAllCourses } from "../controllers/course.controller.js";
 
 const router = Router();
 
-router.route("/addCourse").post(addCourse);
+router.route("/addCourse").post(
+  upload.fields([
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  addCourse
+);
 router.route("/getallCourses").get(getAllCourses);
 
 export default router;
