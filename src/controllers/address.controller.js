4fcaadcd0379876e865c8ef0f7ac763e3fccb22a
Yasowant <yasowant1998@gmail.com>
2024-12-13 01:59:59 +0530

    update address api done

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -69,6 +69,58 @@ const getAddressesForUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, addresses, "Addresses retrieved successfully"));
 });
 
+const updateAddress = asyncHandler(async (req, res) => {
+  //take the id from params
+  //and add all the fild is update
+  //only validation for default address
+  //and after the save the updated addres / set the value
+  const { id } = req.params;
+  const {
+    fullName,
+    mobileNumber,
+    pinCode,
+    houseNumber,
+    area,
+    landMark,
+    city,
+    state,
+    defaultAddress,
+  } = req.body;
+
+  if (defaultAddress !== undefined && typeof defaultAddress !== "boolean") {
+    throw new ApiError(400, "defaultAddress must be a boolean");
+  }
+  const address = await AddressCollection.findById(id);
+  if (!address) {
+    throw new ApiError(404, "Address not found");
+  }
+
+  const fieldsToUpdate = {
+    fullName,
+    mobileNumber,
+    pinCode,
+    houseNumber,
+    area,
+    landMark,
+    city,
+    state,
+    defaultAddress,
+  };
+
+  // Update address fields only if they are provided
+  Object.keys(fieldsToUpdate).forEach((field) => {
+    if (fieldsToUpdate[field] !== undefined) {
+      address[field] = fieldsToUpdate[field];
+    }
+  });
+
+  await address.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, address, "Address updated successfully"));
+});
+
 const deleteAddress = asyncHandler(async (req, res) => {
   const { id } = req.params;
 
@@ -83,4 +135,4 @@ const deleteAddress = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, null, "Address deleted successfully"));
 });
 
-export { addAddress, getAddressesForUser, deleteAddress };
+export { addAddress, getAddressesForUser, updateAddress, deleteAddress };
--- src/routes/address.routes.js
+++ src/routes/address.routes.js
@@ -3,12 +3,14 @@ import {
   addAddress,
   deleteAddress,
   getAddressesForUser,
+  updateAddress,
 } from "../controllers/address.controller.js";
 
 const router = Router();
 
 router.route("/addaddress").post(addAddress);
-router.route("/deleteaddress/:id").delete(deleteAddress);
 router.route("/user/:userId/addresses").get(getAddressesForUser);
+router.route("/deleteaddress/:id").delete(deleteAddress);
+router.route("/updateaddress/:id").put(updateAddress);
 
 export default router;
